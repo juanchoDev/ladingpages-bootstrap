b4a574f63e39850e7efc712b2696558db208bfd3
Juan Escobar <soportec01@corpoplast.local>
2024-05-22 09:59:41 -0400

    some changes

---
+++ readme.txt
@@ -0,0 +1,1 @@
+landing page desarrollada con Bootstrap. 
---
+++ scripts.js
@@ -0,0 +1,57 @@
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+
+card1.innerHTML = `<div class="card">
+<img
+  src="./img/raiting.png"
+  alt="Raiting"
+  class="card-img card-img-filter"
+/>
+<div
+  class="card-img-overlay text-white d-flex flex-column justify-content-center text-center"
+>
+  <h4>Rafting</h4>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+    animi rerum facilis officia in sint.
+  </p>
+  <a href="#" class="text-reset">Leer más</a>
+</div>
+</div>`
+
+card2.innerHTML = `<div class="card">
+<img
+  src="./img/hiking.png"
+  alt="Hiking"
+  class="card-img card-img-filter"
+/>
+<div
+  class="card-img-overlay text-white d-flex flex-column justify-content-center text-center"
+>
+  <h4>Hiking</h4>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+    animi rerum facilis officia in sint.
+  </p>
+  <a href="#" class="text-reset">Leer más</a>
+</div>
+</div>`
+
+card3.innerHTML = `<div class="card">
+<img
+  src="./img/camping.png"
+  alt="Camping"
+  class="card-img card-img-filter"
+/>
+<div
+  class="card-img-overlay text-white d-flex flex-column justify-content-center text-center"
+>
+  <h4>Camping</h4>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+    animi rerum facilis officia in sint.
+  </p>
+  <a href="#" class="text-reset">Leer más</a>
+</div>
+</div>`
